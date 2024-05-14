15c7bc1f7937482cc50d19d014a73a974401fa01
adriesco <144839808+adriesco@users.noreply.github.com>
2024-05-14 23:43:52 +0200

    .

--- Proyecto3Eva/src/main/ByteCode.java
+++ Proyecto3Eva/src/main/ByteCode.java
@@ -1,4 +1,3 @@
-
 package main;
 
 public class ByteCode {
@@ -7,6 +6,19 @@ public class ByteCode {
 
 	public ByteCode(ENUM_BYTECODE bc) {
 		this.name = bc;
+		this.param = -1;
 	}
 	
+	public ByteCode(ENUM_BYTECODE bc, int i) {
+		this.name = bc;
+		this.param = i;
+	}
+	
+	public ENUM_BYTECODE getBC() {
+		return this.name;
+	}
+	
+	public int getParam() {
+		return this.param;
+	}
 }
--- Proyecto3Eva/src/main/ByteCodeParser.java
+++ Proyecto3Eva/src/main/ByteCodeParser.java
@@ -1,4 +1,3 @@
-
 package main;
 
 public class ByteCodeParser {
@@ -6,30 +5,31 @@ public class ByteCodeParser {
 	public static ByteCode parse(String s) {
 		String[] a = s.split(" ");
 		switch (a.length) {
-		case 3:
-			if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("push")) {
-				return new ByteCode(ENUM_BYTECODE.PUSH);
-			}else if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("load")) {
-				return new ByteCode(ENUM_BYTECODE.LOAD);
-			}else if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("store")) {
-				return new ByteCode(ENUM_BYTECODE.STORE);
+		case 1:
+			if (a[0].equalsIgnoreCase("PUSH")) {
+				return new ByteCode(ENUM_BYTECODE.PUSH, Integer.parseInt(a[1]));
+			}else if (a[0].equalsIgnoreCase("LOAD")) {
+				return new ByteCode(ENUM_BYTECODE.LOAD, Integer.parseInt(a[1]));
+			}else if (a[0].equalsIgnoreCase("STORE")) {
+				return new ByteCode(ENUM_BYTECODE.STORE, Integer.parseInt(a[1]));
 			}
 		case 2:
-			if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("add")) {
+			if(a[0].equalsIgnoreCase("add")) {
 				return new ByteCode(ENUM_BYTECODE.ADD);
-			}else if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("div")) {
+			}else if (a[0].equalsIgnoreCase("DIV")) {
 				return new ByteCode(ENUM_BYTECODE.DIV);
-			}else if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("mul")) {
+			}else if (a[0].equalsIgnoreCase("MUL")) {
 				return new ByteCode(ENUM_BYTECODE.MUL);
-			}else if (a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("sub")) {
+			}else if (a[0].equalsIgnoreCase("SUB")) {
 				return new ByteCode(ENUM_BYTECODE.SUB);
-			}else if(a[0].equalsIgnoreCase("newinst") && a[1].equalsIgnoreCase("out")) {
+			}else if(a[0].equalsIgnoreCase("OUT")) {
 				return new ByteCode(ENUM_BYTECODE.OUT);
-			}else if (a[0].equalsIgnoreCase("newinst") &&a[1].equalsIgnoreCase("halt")) {
+			}else if (a[0].equalsIgnoreCase("HALT")) {
 				return new ByteCode(ENUM_BYTECODE.HALT);
 			}
 		default:
 			return null;
 		}
 	}
+
 }
