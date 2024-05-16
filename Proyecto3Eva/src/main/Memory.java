ced64e24cce2642c3819b52dd8f68149e365ebed
adriesco <144839808+adriesco@users.noreply.github.com>
2024-05-16 17:03:16 +0200

    .

--- Proyecto3Eva/src/main/Memory.java
+++ Proyecto3Eva/src/main/Memory.java
@@ -2,81 +2,66 @@ package main;
 
 public class Memory {
 	private int Max_memory;
-	private int size = 5;
-	private Integer[] memory = new Integer[size];
+	private int size;
+	private Integer[] memory;
 	private boolean empty;
-
-	/**
-	 * Constructor de la clase Memory.
-	 */
+	
 	public Memory() {
+		size = 5;
+		memory = new Integer[size];
 		this.empty = true;
 	}
-
-	/**
-	 * Métodopara redimensionar el array de memoria si es necesario.
-	 * 
-	 * @param pos La posición a la que se intenta acceder en la memoria.
-	 */
-	private void resize(int pos) {
-		if (pos >= size) {
+	
+	private void resize (int pos) {
+		if(pos >= size) {
 			this.empty = false;
-			Integer[] array2 = new Integer[pos * 2];
+			Integer[] array2 = new Integer[pos*2];
 			for (int i = 0; i < this.memory.length; i++) {
-				array2[i] = this.memory[i];
-			}
+				if(i < this.size) {
+					array2[i] = this.memory[i];
+				}else {
+					array2[i] = null;
+				}
+	        }
 			this.memory = array2;
+			this.size = array2.length;
 		}
 	}
-
-	/**
-	 * Escribe un entero en la posición especificada de la memoria.
-	 * 
-	 * @return true si se escribe , false si la posición es inválida.
-	 */
-	public boolean write(int pos, int x) {
-		if (pos >= 0) {
+	
+	public boolean write(int pos, Integer x) {
+		if(pos >= 0) {
 			this.resize(pos);
 			this.memory[pos] = x;
 			return true;
-		} else {
+		}else {
 			return false;
 		}
 	}
-
-	/**
-	 * Lee el numero almacenado en la posición especificada de la memoria.
-	 * 
-	 * @param pos La posición de la que se desea leer el numero.
-	 * @return El entero almacenado en la posición especificada
-	 */
+	
 	public Integer read(int pos) {
 		if (this.memory[pos] == null) {
 			return -1;
-		} else {
+		}else {
 			return this.memory[pos];
 		}
 	}
-
-	/**
-	 * Retorna una representación en cadena de la memoria.
-	 * 
-	 * @return Una cadena que representa los enteros almacenados en la memoria,
-	 *         separados por espacios, o null si la memoria está vacía.
-	 */
+	
 	public String toString() {
-		String texto = "";
-		if (!empty) {
-			for (int i = 0; i < memory.length; i++) {
-				if (this.memory[i] == null) {
-
-				} else {
-					texto += this.memory[i] + " ";
+			String texto = "";
+			if (empty == false) {
+				for(int i = 0; i < memory.length; i++) {
+					if (this.memory[i] == null) {
+						
+					}else {
+						texto += "[" + i + "]" + this.memory[i] + " ";
+					}
 				}
-			}
-			return texto;
-		} else {
-			return null;
-		}
+				return texto;
+			}else
+				return "vacia";
+	}
+	
+	public void erase() {
+		this.memory = new Integer[size];
 	}
 }
